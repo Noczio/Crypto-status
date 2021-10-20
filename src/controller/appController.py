cc99ffddad5bea3e98ba7d3ab25e6370be948877
Nocz <devnocz@gmail.com>
2021-10-20 14:06:53 -0500

    tested

--- src/controller/appController.py
+++ src/controller/appController.py
@@ -23,13 +23,14 @@ class AppController:
             rows = table.find_all('td')
 
             information = format_table(rows)
-            current_coin = Coin(search_name.capitalize(), *information)
+            current_coin = Coin(search_name, *information)
 
             if index == number_of_elements - 1:
                 print(current_coin)
             else:
                 print(current_coin, end="\n\n")
 
-            file_name = current_coin.name + " " + current_coin.time + ".json"
-            result = JsonFile(path="out", file=file_name)
+            file_name = current_coin.time + ".json"
+            path = "out/" + current_coin.name
+            result = JsonFile(path, file_name)
             result.save(current_coin.__dict__)
