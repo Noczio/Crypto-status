8cebe952f97416bc23d5f9c71295731484a806eb
Nocz <devnocz@gmail.com>
2021-10-29 11:03:37 -0500

    error catch

--- src/controller/appController.py
+++ src/controller/appController.py
@@ -16,21 +16,24 @@ class AppController:
         number_of_elements = len(self.arguments["search"])
 
         for index in range(number_of_elements):
-            search_name = self.arguments["search"][index]
-            web_request = requests.get(self.arguments["url"] + "/currencies/" + search_name)
-            content = BeautifulSoup(web_request.content, 'lxml')
-            table = content.findChildren('table')[0]
-            rows = table.find_all('td')
+            try:
+                search_name = self.arguments["search"][index]
+                web_request = requests.get(self.arguments["url"] + "/currencies/" + search_name)
+                content = BeautifulSoup(web_request.content, 'lxml')
+                table = content.findChildren('table')[0]
+                rows = table.find_all('td')
 
-            information = format_table(rows)
-            current_coin = Coin(search_name, *information)
+                information = (search_name,) + format_table(rows)
+                current_coin = Coin(*information)
 
-            if index == number_of_elements - 1:
-                print(current_coin)
-            else:
-                print(current_coin, end="\n\n")
+                if index == number_of_elements - 1:
+                    print(current_coin)
+                else:
+                    print(current_coin, end="\n\n")
 
-            file_name = current_coin.time + ".json"
-            path = "out/" + current_coin.name
-            result = JsonFile(path, file_name)
-            result.save(current_coin.__dict__)
+                file = current_coin.time + ".json"
+                path = "out/" + current_coin.name
+                result = JsonFile(path, file)
+                result.save(current_coin.__dict__)
+            except Exception as error:
+                print(f"Error: {error}")
